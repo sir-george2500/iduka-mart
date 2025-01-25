2c5c4be95e801f16add8b9dbf56703453a530939
George S. Mulbah <gsmulbah2500@gmail.com>
2025-01-25 05:11:31 +0200

    added the cart icon to the navbar

--- apps/iduka-mart_frontend/src/app/layout/navbar/navbar.component.ts
+++ apps/iduka-mart_frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'iduka-mart-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent { }
--- apps/iduka-mart_frontend/src/app/shared/font-awesome-icons.ts
+++ apps/iduka-mart_frontend/src/app/shared/font-awesome-icons.ts
@@ -1,7 +1,7 @@
 import { IconDefinition } from '@fortawesome/angular-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
-import { faHome, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faHome, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 export const fontAwesomeIcons: IconDefinition[] = [
-  faUser, faShoppingCart, faHome
+  faUser, faShoppingCart, faHome, faCartShopping
 ];
