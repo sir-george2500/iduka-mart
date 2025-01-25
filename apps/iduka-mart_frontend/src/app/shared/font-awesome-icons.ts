449e6ffa46c14cfab8b19eee98d44678af9cb139
George S. Mulbah <gsmulbah2500@gmail.com>
2025-01-25 05:44:19 +0200

    completed the initial design for the navigation bar

--- apps/iduka-mart_frontend/src/app/shared/font-awesome-icons.ts
+++ apps/iduka-mart_frontend/src/app/shared/font-awesome-icons.ts
@@ -1,7 +1,6 @@
 import { IconDefinition } from '@fortawesome/angular-fontawesome';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
-import { faCartShopping, faHome, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faHome, faShoppingCart, faUser, } from '@fortawesome/free-solid-svg-icons';
 
 export const fontAwesomeIcons: IconDefinition[] = [
-  faUser, faShoppingCart, faHome, faCartShopping
+  faUser, faShoppingCart, faHome, faCartShopping,
 ];
