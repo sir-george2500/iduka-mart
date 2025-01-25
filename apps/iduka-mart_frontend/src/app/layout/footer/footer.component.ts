ea3ea915b1c526010b7c38f57e4c555c34524ff8
George S. Mulbah <gsmulbah2500@gmail.com>
2025-01-25 11:24:47 +0200

    added the truck icon to the footer

--- apps/iduka-mart_frontend/src/app/layout/footer/footer.component.ts
+++ apps/iduka-mart_frontend/src/app/layout/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'iduka-mart-footer',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent { }
--- apps/iduka-mart_frontend/src/app/shared/font-awesome-icons.ts
+++ apps/iduka-mart_frontend/src/app/shared/font-awesome-icons.ts
@@ -1,6 +1,14 @@
 import { IconDefinition } from '@fortawesome/angular-fontawesome';
-import { faCartShopping, faHome, faShoppingCart, faUser, } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faCartShopping, faHome, faShoppingCart, faTruckFast, faUser } from '@fortawesome/free-solid-svg-icons';
 
 export const fontAwesomeIcons: IconDefinition[] = [
-  faUser, faShoppingCart, faHome, faCartShopping,
+  faUser,
+  faShoppingCart,
+  faHome,
+  faCartShopping,
+  faTruckFast,
+  faFacebook,
+  faTwitter,
+  faYoutube
 ];
